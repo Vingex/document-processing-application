b5987f7a5ebbc1610956ebc88138e6afd1f4c48b
Alexey <alex.ving@mail.ru>
2016-10-19 13:45:12 +0500

    Create Staffs and JaxbParser

---
+++ src/main/java/Parsers/JaxbParser.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Parsers;
+
+import java.io.File;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+/**
+ *
+ * @author Алексей
+ */
+public class JaxbParser implements Parser{
+    
+    @Override
+    public Object getObject(File file, Class c) throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(c);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        Object object = unmarshaller.unmarshal(file);
+ 
+        return object;
+    }
+ 
+    @Override
+    public void saveObject(File file, Object o) throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(o.getClass());
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.marshal(o,file);
+    }
+}
